930b4da322d41a002ba61d7a5f656cc81e7cb740
paolo21d <paolo21d@gmail.com>
2018-12-13 23:34:17 +0100

    Testowanie: czasy dodawania i wyszukiwania elementow w TreeMap i HashMap

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,14 @@
 #include <cstddef>
 #include <cstdlib>
 #include <string>
+#include <iostream>
+#include<chrono>
 
 #include "TreeMap.h"
+#include "HashMap.h"
 
 namespace
 {
-
 template <typename K, typename V>
 using Map = aisdi::TreeMap<K, V>;
 
@@ -20,8 +22,46 @@ void perfomTest()
 
 int main(int argc, char** argv)
 {
-  const std::size_t repeatCount = argc > 1 ? std::atoll(argv[1]) : 10000;
-  for (std::size_t i = 0; i < repeatCount; ++i)
-    perfomTest();
-  return 0;
+  const std::size_t repeatCount = argc > 1 ? std::atoll(argv[1]) : 1000000;
+  /*for (std::size_t i = 0; i < repeatCount; ++i)
+    perfomTest();*/
+
+	aisdi::TreeMap<int, int> treeMap;
+	aisdi::HashMap<int, int> hashMap;
+
+	//auto start = std::chrono::system_clock::now();
+	//auto done = std::chrono::system_clock::now();
+	//std::cout << "LinkedList preppend time: " << (done - start).count() << '\n';
+
+	//Dodanie do TreeMap
+	auto start = std::chrono::system_clock::now();
+	for (std::size_t i = 0; i < repeatCount; ++i) {
+		treeMap[i] = i;
+	}
+	auto done = std::chrono::system_clock::now();
+	std::cout << "TreeMap dodanie elementow: " << (done - start).count() << '\n';
+	//Dodanie do HashMap
+	start = std::chrono::system_clock::now();
+	for (std::size_t i = 0; i < repeatCount; ++i) {
+		hashMap[i] = i;
+	}
+	done = std::chrono::system_clock::now();
+	std::cout << "HashMap dodanie elementow: " << (done - start).count() << '\n';
+
+
+	///Wyszukiwanie w TreeMap
+	start = std::chrono::system_clock::now();
+	for (std::size_t i = 0; i < repeatCount; ++i) {
+		treeMap.find(i);
+	}
+	done = std::chrono::system_clock::now();
+	std::cout << "TreeMap wyszukiwanie elementow: " << (done - start).count() << '\n';
+	///Wyszukiwanie w HashMap
+	start = std::chrono::system_clock::now();
+	for (std::size_t i = 0; i < repeatCount; ++i) {
+		hashMap.find(i);
+	}
+	done = std::chrono::system_clock::now();
+	std::cout << "HashMap wyszukiwanie elementow: " << (done - start).count() << '\n';
+
 }
