7b1440288f07be0009e8e6a265b0d4d5c3083629
paolo21d <paolo21d@gmail.com>
2018-12-15 00:17:01 +0100

    Zmienic jeszcze end()

--- src/main.cpp
+++ src/main.cpp
@@ -16,33 +16,41 @@ using Hash = aisdi::HashMap<K, V>;
 
 void testTree(const std::size_t repeatCount) {
 	Tree<int, int> treeMap;
-	auto start = std::chrono::system_clock::now();
+	//auto start = std::chrono::system_clock::now();
 	for (std::size_t i = 0; i < repeatCount; ++i) {
 		treeMap[i] = i;
 	}
+	auto start = std::chrono::system_clock::now();
+	treeMap[repeatCount] = repeatCount;
 	auto done = std::chrono::system_clock::now();
 	std::cout << "TreeMap dodanie elementow: " << (done - start).count() << '\n';
 	//Wyszukiwanie
-	start = std::chrono::system_clock::now();
-	for (std::size_t i = 0; i < repeatCount; ++i) {
+	//start = std::chrono::system_clock::now();
+	/*for (std::size_t i = 0; i < repeatCount; ++i) {
 		treeMap.find(i);
-	}
+	}*/
+	start = std::chrono::system_clock::now();
+	treeMap.find(repeatCount);
 	done = std::chrono::system_clock::now();
 	std::cout << "TreeMap wyszukiwanie elementow: " << (done - start).count() << '\n';
 }
 void testHash(const std::size_t repeatCount) {
 	Hash<int, int> hashMap;
-	auto start = std::chrono::system_clock::now();
+	//auto start = std::chrono::system_clock::now();
 	for (std::size_t i = 0; i < repeatCount; ++i) {
 		hashMap[i] = i;
 	}
+	auto start = std::chrono::system_clock::now();
+	hashMap[repeatCount] = repeatCount;
 	auto done = std::chrono::system_clock::now();
 	std::cout << "HashMap dodanie elementow: " << (done - start).count() << '\n';
 	//Wyszukiwanie
-	start = std::chrono::system_clock::now();
-	for (std::size_t i = 0; i < repeatCount; ++i) {
+	//start = std::chrono::system_clock::now();
+	/*for (std::size_t i = 0; i < repeatCount; ++i) {
 		hashMap.find(i);
-	}
+	}*/
+	start = std::chrono::system_clock::now();
+	hashMap.find(repeatCount);
 	done = std::chrono::system_clock::now();
 	std::cout << "HashMap wyszukiwanie elementow: " << (done - start).count() << '\n';
 }
